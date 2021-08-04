fdbefe72d4b5578e5f2183962657aba4ef83e958
Jsakkos <jonathansakkos@protonmail.com>
2021-08-04 13:34:53 -0400

    Update utils.py

--- src/nufeb_tools/utils.py
+++ src/nufeb_tools/utils.py
@@ -49,7 +49,7 @@ class get_data:
         elif directory:
             self.directory = directory
             self.get_local_data()
-            self.sucRatio = int(self.directory.split('_')[-3])
+            self.sucRatio = int(self.directory.split('_')[-4])
         elif id:
             self.id = id
             self.get_datafed_data()
